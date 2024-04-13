6efac7ca58e2ffef6c33353f3443846d759bdedc
Aliakse Yausiuk <Aliakseyausiuk@gmail.com>
2024-04-13 12:03:54 +0300

    feat: add app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,27 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component } from "@angular/core";
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
+import { RouterOutlet } from "@angular/router";
+
+import { SideBarComponent } from "./side-bar/side-bar.component";
+import { CommonModule } from "@angular/common";
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+	selector: "students-details-root",
+	standalone: true,
+	imports: [
+		RouterOutlet,
+		FormsModule,
+		ReactiveFormsModule,
+		SideBarComponent,
+		CommonModule,
+	],
+	templateUrl: "./app.component.html",
+	styleUrl: "./app.component.scss",
 })
 export class AppComponent {
-  title = 'students-details';
+	title = "students-details";
+
+	sidebarExpanded: boolean = true;
+
+	constructor() {}
 }
