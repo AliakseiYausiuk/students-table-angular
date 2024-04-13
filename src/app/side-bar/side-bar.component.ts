2372aa1a77e84a482e49784cded084c7edfbb926
Aliakse Yausiuk <Aliakseyausiuk@gmail.com>
2024-04-13 12:04:53 +0300

    feat: add sidebar component

---
+++ src/app/side-bar/side-bar.component.ts
@@ -0,0 +1,18 @@
+import { CommonModule } from "@angular/common";
+import { Component, EventEmitter, Input, Output } from "@angular/core";
+import { RouterModule } from "@angular/router";
+
+@Component({
+	selector: "students-details-side-bar",
+	standalone: true,
+	imports: [CommonModule, RouterModule],
+	templateUrl: "./side-bar.component.html",
+	styleUrl: "./side-bar.component.scss",
+})
+export class SideBarComponent {
+	@Input() isExpanded: boolean = false;
+	@Output() toggleSidebar: EventEmitter<boolean> =
+		new EventEmitter<boolean>();
+
+	handleSidebarToggle = () => this.toggleSidebar.emit(!this.isExpanded);
+}
