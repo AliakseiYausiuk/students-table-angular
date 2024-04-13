8c57930c5983dec6a004c5be6519b3b3dee862f7
Aliakse Yausiuk <Aliakseyausiuk@gmail.com>
2024-04-13 12:04:13 +0300

    feat: add routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
-import { Routes } from '@angular/router';
+import { Routes } from "@angular/router";
+import { StudentRecordsComponent } from "./student-records/student-records.component";
+import { LandingComponent } from "./landing/landing.component";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+	{ path: "landing", component: LandingComponent },
+	{ path: "students", component: StudentRecordsComponent },
+];
